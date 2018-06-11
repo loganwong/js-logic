31b8374347df65c19e59e27df8de928f4096a4da
BRIAN   AND DONNA WONG <loganwong@student.midpac.edu>
2018-06-11 13:48:40 -1000

    Exercise #4 complete

--- exercises.js
+++ exercises.js
@@ -95,6 +95,14 @@ console.log(isGreaterThan(3, 1));
  * Console.log your result.
 */
 
+function mustBeTrue(boo) {
+  if(boo === true){
+    return true;
+  }else{
+    return false;
+  }
+}
+console.log("mustBeTrue: ", mustBeTrue(true));
 
 
 /*
