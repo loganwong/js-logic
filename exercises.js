a9b7d12e04f9095d1cdc3f50e76e368f8e869cf7
BRIAN   AND DONNA WONG <loganwong@student.midpac.edu>
2018-06-11 14:14:08 -1000

    Finished exercise #10

--- exercises.js
+++ exercises.js
@@ -229,8 +229,15 @@ console.log(dirty30(14, 14, 1));
  * Console.log your result.
 */ 
 
-
-
+function evenStevens(num) {
+  if(num % 2 === 0){
+    return true;
+  }else{
+    return false;
+  }
+}
+console.log(evenStevens(2));
+console.log(evenStevens(3));
 
 /*
  * #11
